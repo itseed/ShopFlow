fd3c6ffb0d774e71e6df31d0b74850ccfa830d65
Kriangkrai Intarangsri <intarangsri@gmail.com>
2025-07-23 02:54:43 +0700

    update landing page

---
+++ landing/script.js
@@ -0,0 +1,12 @@
+// Smooth scroll for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    const href = this.getAttribute('href');
+    if (href.length > 1 && document.querySelector(href)) {
+      e.preventDefault();
+      document.querySelector(href).scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+}); 
